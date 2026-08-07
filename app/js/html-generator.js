254c0d9e9246e145b4ed7ea9fecffb618602b01d
LateLammas <latelamas@gmail.com>
2026-08-07 22:37:50 +0300

    Add bookmark SVG icons for empty and filled states

--- app/js/html-generator.js
+++ app/js/html-generator.js
@@ -149,7 +149,12 @@ ${cssContent}
 
       <!-- Bookmark button -->
       <button id="bookmark-btn" class="icon-btn" aria-label="Tallenna tai poista kirjanmerkki">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+        <!-- empty (outline) bookmark -->
+        <svg id="bookmark-empty" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+          <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+        </svg>
+        <!-- filled (solid) bookmark -->
+        <svg id="bookmark-filled" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" style="display: none;">
           <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
         </svg>
       </button>
